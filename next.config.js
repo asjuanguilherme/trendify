939af92aa7020729b9501f309c68a318bf47cabf
Juan Guilherme <asjuanguilherme@gmail.com>
2023-09-08 17:45:39 -0300

    feat: :sparkles: implements pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
-    styledComponents: true,
-  },
+    styledComponents: true
+  }
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
